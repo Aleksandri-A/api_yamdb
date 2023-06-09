7ebe287764345e53f872ba1445d20d242fa92fd7
Aleksandri-A <123542168+Aleksandri-A@users.noreply.github.com>
2023-06-09 17:54:20 +0700

    Update admin.py

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,11 +1,6 @@
 from django.contrib import admin
 
-from reviews.models import Category, Comment, Genre, Title
-
 from .models import User
 
+
 admin.site.register(User)
-admin.site.register(Genre)
-admin.site.register(Category)
-admin.site.register(Title)
-admin.site.register(Comment)
